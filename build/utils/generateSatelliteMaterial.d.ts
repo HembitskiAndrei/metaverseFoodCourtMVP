caa3cb7c559641914ce68f8f92bce65b38f9c30f
HembitskiAndrei <shaftdg3@gmail.com>
2022-07-24 18:44:37 +0300

    feat: bar light

--- build/utils/generateSatelliteMaterial.d.ts
+++
@@ -1,2 +0,0 @@
-import { Mesh, Scene } from "@babylonjs/core";
-export declare const generateSatelliteMaterial: (root: Mesh, others: Mesh[], scene: Scene) => void;
--- build/utils/infoAnimation.d.ts
+++
@@ -1,3 +0,0 @@
-import { Rectangle } from "@babylonjs/gui";
-import { AnimationGroup } from "@babylonjs/core";
-export declare const InfoAnimation: (object: Rectangle, currentScaleValue: number, targetScaleValue: number, duration: number) => AnimationGroup;
